58c0012ac925a39aad2e93a7eafbd4c25ef7a648
KamZet <37073291+KamZet@users.noreply.github.com>
2019-12-16 13:50:57 +0000

    Updated headers
    Interfaces of the TCP server and file manager are now uploaded.

--- include/RequestServer.h
+++ include/RequestServer.h
@@ -5,10 +5,6 @@
 #ifndef SIMPLE_P2P_REQUEST_SERVER_H
 #define SIMPLE_P2P_REQUEST_SERVER_H
 
-#include <memory>	// make_shared
-#include <utility>	// move
-#include <boost/asio.hpp>
-
 #include "RequestWorker.h"
 
 using boost::asio::ip::tcp;
@@ -18,24 +14,9 @@ namespace SimpleP2P {
 	private:
 		tcp::acceptor _acceptor;
 		
-		void accept_connection() {
-			_acceptor.async_accept(
-				// Accept handler.
-				[this] (boost::system::error_code err, tcp::socket socket) {
-					if (!err){
-						std::make_shared<RequestWorker>(std::move(socket))->start();
-					}
-					
-					accept_connection(); // Wait for another request.
-				});
-			}
-	
+		void accept_connection();
 	public:
-		RequestServer(boost::asio::io_context& io_context, short port)
-			: _acceptor(io_context, tcp::endpoint(tcp::v4(), port)) {
-				accept_connection();
-		}
-	}
+		RequestServer(boost::asio::io_context& io_context, short port);
 }
 						
 #endif // SIMPLE_P2P_REQUEST_SERVER_H	
