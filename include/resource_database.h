30cae425169a6c82142298be95e927551f74a4e9
Rodzyn <przemek.stawczyk@gmail.com>
2020-01-03 15:48:20 +0100

    Resource database (#6)

--- include/resource_database.h
+++ include/resource_database.h
@@ -28,11 +28,10 @@ namespace simpleP2P {
          * @param res Resource to be checked
          * @return true if host already has some resource
          */
-        //TODO
-        bool has_file(Resource res);
+        bool has_file(const Resource &res);
 
         /* NOTE: chosen conception
-         * >>> use pointers outside Database -> slightly inefficient, can't track life as it's not dynamically allocated
+         * >>> use pointers outside Database -> slightly inefficient,
          *      probably best as it is possible to have host without resources and theoretically lonely resource
          * This assumes that once host is in database it's never deleted
          */
@@ -55,21 +54,46 @@ namespace simpleP2P {
                          const Host &host);
 
         /**
+         * Updates the list of resources aviable from host
+         * Triggered after receive of full Beacon Packet
+         * @param host Host and possesed resources in a struct
+         */
+        void update_host(const Host &host);
+
+        /**
+         * Revokes resource and disconnects it from Hosts in database and database itself
+         * Resource will still point to Hosts that possess it
+         * @param resource Resource to be revoked
+         */
+        void revoke_resource(const Resource &resource);
+
+        /**
          * same as add_file(Resource, Host) but host is localhost
          * @param res Resource to be added
          */
         inline void add_file(const Resource &res);
 
         /**
-         * same as remove_file(Resource, Host) but host is localhost
-         * @param res Resource to be removed from localhost list
-         * @return returns false if file did not existed or was not possesed
-         */
+          * same as remove_file(Resource, Host) but host is localhost
+          * @param res Resource to be removed from localhost list
+          * @return returns false if file did not existed or was not possesed
+          */
         inline bool remove_file(const Resource &res);
 
-        // TODO: consider returning shared_ptr for Resource
-        std::vector<Host *> who_has_file(std::vector<Int8> resource_header);
-        std::vector<Host *> who_has_file(const Resource &res);
+        /**
+        * Returns shared pointer to resource to allow access to information about file owners
+        * @param res Resource about which information is gathered
+        * @return shared pointer to res
+        */
+        inline std::shared_ptr<Resource>
+        who_has_file(std::vector<Int8> resource_header); //NOTE: no constructor available
+
+        /**
+         * Returns shared pointer to resource to allow access to information about file owners
+         * @param res Resource about which information is gathered
+         * @return shared pointer to res
+         */
+        std::shared_ptr<Resource> who_has_file(const Resource &res);
 
         /**
          * Generates listing of localhost content in a header
@@ -80,8 +104,8 @@ namespace simpleP2P {
     private:
         Host my_host;                     //!< localhost Host struct
         /* all internal operation on this vectors must be made with pointers */
-        std::vector<Resource> resources;  //!< vector of Resources in database
-        std::vector<Host> hosts;          //!< vector of Hosts in database
+        std::vector<std::shared_ptr<Resource>> resources;  //!< vector of Resources in database
+        std::vector<std::shared_ptr<Host>> hosts;          //!< vector of Hosts in database
         std::shared_mutex database_mutex; //!< rw_lock for database, allows multiple concurrent reads but permits concurrent writes
     };
 }
--- src/resource.cpp
+++ src/resource.cpp
@@ -9,16 +9,16 @@
 
 namespace simpleP2P {
     Resource::Resource(std::string name_c, Uint64 size_c, std::string path_c)
-            : size(size_c), name(name_c), path(path_c) {}
+            : size(size_c), name(name_c), invalidated(false), path(path_c) {}
 
-    Resource::Resource(std::vector<Int8> resource_header) {
+    Resource::Resource(std::vector<Int8> resource_header) : invalidated(false) {
         (void) resource_header;
         //TODO
     }
 
     std::vector<Int8> Resource::generate_resource_header() {
         std::vector<Int8> header;
-        header.resize(256 + 64);
+        header.resize(256 + sizeof(Uint64));
         memset(header.data(), 0, 256);
         strcpy(reinterpret_cast<char *>(header.data()), getName().c_str());
 
@@ -29,10 +29,10 @@ namespace simpleP2P {
     }
 
     bool Resource::has_host(simpleP2P::Host host) {
-        return std::count_if(host_in_possetion.begin(),
-                             host_in_possetion.end(),
-                             [&host](auto it) {
-                                 return *it == host;
+        return std::count_if(hosts_in_possession.begin(),
+                             hosts_in_possession.end(),
+                             [&host](std::weak_ptr<Host> &it) {
+                                 return *(it.lock().get()) == host;
                              }) != 0;
     }
 
@@ -55,5 +55,23 @@ namespace simpleP2P {
 
     bool Resource::operator!=(const Resource &other) const {
         return !operator==(other);
+    }
+
+    void Resource::remove_host(std::shared_ptr<Host> host) {
+        //make local copy and erase
+        std::vector<std::weak_ptr<Host>> temp(hosts_in_possession.begin(),
+                                              hosts_in_possession.end());
+        temp.erase(
+                std::remove_if(
+                        temp.begin(),
+                        temp.end(),
+                        [&host](auto &it) {
+                            return it.lock() == host;
+                        }
+                ), temp.end());
+
+        //swap contents of the vectors
+        hosts_in_possession.clear();
+        hosts_in_possession.assign(temp.begin(), temp.end());
     }
 }
